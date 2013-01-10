41947c7bbb37fad3365472445d113b3cb6b64cf2
Will Ross <paxswill@gmail.com>
2013-01-09 21:18:39 -0500

    Consolidate all placeholder pages together
    This also means future placeholder can fairly easily be mocked up.

--- ybtheta.py
+++ ybtheta.py
@@ -4,27 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('placeholder.html', name='home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('placeholder.html', name='about')
 
 @app.route('/rush')
 def rush():
-    return render_template('rush.html')
+    return render_template('placeholder.html', name='rush')
 
 @app.route('/brothers')
 def brothers():
-    return render_template('brothers.html')
+    return render_template('placeholder.html', name='brothers')
 
 @app.route('/activities')
 def activities():
-    return render_template('activities.html')
+    return render_template('placeholder.html', name='activities')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('placeholder.html', name='contact')
 
 if __name__ == '__main__':
     app.debug = True
