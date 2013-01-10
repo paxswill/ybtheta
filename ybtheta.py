38da23e2809f3fa1e2ca3cc0d35b3050738209ad
Will Ross <paxswill@gmail.com>
2013-01-10 01:08:41 -0500

    Fill in the About page with ported content
    I'm planning on using static markdown until I get a more capable CMS-like
    system in place. Right now About is just filled with a bunch of placeholder
    text.

--- ybtheta.py
+++ ybtheta.py
@@ -1,3 +1,7 @@
+import os
+import codecs
+
+from markdown import markdown
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,7 +12,8 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('placeholder.html', name='about')
+    mdown_html = markdown_file('content/about.mdown')
+    return render_template('safe_content.html', name='about', content=mdown_html)
 
 @app.route('/rush')
 def rush():
@@ -25,6 +30,17 @@ def activities():
 @app.route('/contact')
 def contact():
     return render_template('placeholder.html', name='contact')
+
+def markdown_file(path, relative=True):
+    if relative:
+        script_dir = os.path.dirname(__file__)
+        real_path = os.path.join(script_dir, path)
+    else:
+        real_path = path
+    md_file = codecs.open(real_path, mode='r', encoding='utf-8')
+    md_text = md_file.read()
+    return markdown(md_text, output_format='html5', safe_mode='escape')
+
 
 if __name__ == '__main__':
     app.debug = True
