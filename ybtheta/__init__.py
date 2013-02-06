5028e6610c7737fd297e0e5e77749b2203ca2c77
Will Ross <paxswill@gmail.com>
2013-02-06 15:30:19 -0500

    Refactor the markdown pages into a Blueprint

--- ybtheta/__init__.py
+++ ybtheta/__init__.py
@@ -1,11 +1,8 @@
-import os
-import codecs
-import sys
-
-from markdown import markdown
 from flask import Flask, render_template, g, session, redirect, flash, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext import openid
+
+from ybtheta.markdown_page import markdown_page
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('settings.cfg')
@@ -15,15 +12,13 @@ db = SQLAlchemy(app)
 
 from ybtheta import accounts
 
+# Register Blueprints
+app.register_blueprint(markdown_page)
+
 # Flask Routes
 @app.route('/')
 def home():
     return render_template('placeholder.html', name='home')
-
-@app.route('/about')
-def about():
-    mdown_html = markdown_file('content/about.mdown')
-    return render_template('safe_content.html', name='about', content=mdown_html)
 
 @app.route('/rush')
 def rush():
@@ -43,16 +38,6 @@ def contact():
 
 
 # Misc functions
-def markdown_file(path, relative=True):
-    if relative:
-        script_dir = os.path.dirname(__file__)
-        real_path = os.path.join(script_dir, path)
-    else:
-        real_path = path
-    md_file = codecs.open(real_path, mode='r', encoding='utf-8')
-    md_text = md_file.read()
-    return markdown(md_text, output_format='html5', safe_mode='escape')
-
 
 if __name__ == '__main__':
     app.run()
---
+++ ybtheta/markdown_page.py
@@ -0,0 +1,29 @@
+import os
+import codecs
+
+from markdown import markdown
+from flask import Blueprint, render_template, abort
+
+markdown_page = Blueprint('markdown_page', __name__)
+
+
+@markdown_page.route('/<page>')
+def render_markdown(page):
+    try:
+        mdown_html = markdown_file('content/{}.mdown'.format(page))
+        return render_template('safe_content.html', name='page',
+                content=mdown_html)
+    except IOError:
+        return abort(404)
+
+
+def markdown_file(path, relative=True):
+    if relative:
+        script_dir = os.path.dirname(__file__)
+        real_path = os.path.join(script_dir, path)
+    else:
+        real_path = path
+    md_file = codecs.open(real_path, mode='r', encoding='utf-8')
+    md_text = md_file.read()
+    return markdown(md_text, output_format='html5', safe_mode='escape')
+
