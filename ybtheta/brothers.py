33f6f28dfed7bcea527163d8df96ec4b60efe3d1
Will Ross <paxswill@gmail.com>
2013-02-07 22:44:58 -0500

    Revising the brother table structure

--- ybtheta/brothers.py
+++ ybtheta/brothers.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from datetime import date, timedelta
+from datetime import date, datetime
 
 from flask import render_template
 from flask.views import View
@@ -56,8 +56,8 @@ class Brother(db.Model):
     picture = db.Column(db.String(200))
     # Theta Tau info
     roll_number = db.Column(db.Integer, nullable=False)
-    page_number = db.Column(db.Integer, nullable=False, unique=True)
-    chapter = db.Column(db.String(100), nullable=False)
+    page_number = db.Column(db.Integer, nullable=False)
+    chapter = db.Column(db.String(100), nullable=False, default="Upsilon Beta")
     initiation = db.Column(db.Date)
     pledge_class = db.Column(db.String(100))
     big_id = db.Column(db.Integer, db.ForeignKey('brother.id'), nullable=True)
@@ -69,7 +69,7 @@ class Brother(db.Model):
     past_positions = db.relationship('Position',
             primaryjoin='(Brother.id == Position.brother_id) & '\
             '(Position.current == False)')
-    status = db.Column(db.Enum('Alumni', 'Student', 'Co-op', 'Inactive',
+    status = db.Column(db.Enum('Alumni', 'Student', 'Co-Op', 'Inactive',
             'Pledge', name='brother_status'), nullable=False,
             default='Student')
     # Academic info
@@ -79,6 +79,10 @@ class Brother(db.Model):
     emails = db.relationship('EmailAddress', backref='brother')
     phone_numbers = db.relationship('PhoneNumber', backref='brother')
     addresses = db.relationship('MailingAddress', backref='brother')
+    # Misc stuff
+    quotes = db.Column(db.Text())
+    revision_timestamp = db.Column(db.DateTime(timezone=True), nullable=False,
+            default=datetime.utcnow())
 
 
 class Position(db.Model):
@@ -105,93 +109,9 @@ class PhoneNumber(db.Model):
     phone_number = db.Column(db.String(30), nullable=False)
 
 
-STATES = {
-        u'Washington': 'WA',
-        u'Oregon': 'OR',
-        u'California': 'CA',
-        u'Nevada': 'NV',
-        u'Hawaii': 'HI',
-        u'Alaska': 'AK',
-        u'Idaho': 'ID',
-        u'Montana': 'MT',
-        u'Wyoming': 'WY',
-        u'Colorado': 'CO',
-        u'New Mexico': 'NM',
-        u'Arizona': 'AZ',
-        u'Utah': 'UT',
-        u'North Dakota': 'ND',
-        u'South Dakota': 'SD',
-        u'Nebraska': 'NE',
-        u'Kansas': 'KS',
-        u'Oklahoma': 'OK',
-        u'Texas': 'TX',
-        u'Arkansas': 'AR',
-        u'Louisiana': 'LA',
-        u'Missouri': 'MO',
-        u'Michigan': 'MI',
-        u'Minnesota': 'MN',
-        u'Wisconsin': 'WI',
-        u'Iowa': 'IA',
-        u'Indiana': 'IN',
-        u'Ohio': 'OH',
-        u'Illinois': 'IL',
-        u'Kentucky': 'KY',
-        u'Tennessee': 'TN',
-        u'Alabama': 'AL',
-        u'Mississippi': 'MS',
-        u'Georgia': 'GA',
-        u'Florida': 'FL',
-        u'South Carolina': 'SC',
-        u'North Carolina': 'NC',
-        u'Virginia': 'VA',
-        u'West Virginia': 'WV',
-        u'Pennsylvania': 'PA',
-        u'New Jersey': 'NJ',
-        u'Maryland': 'MD',
-        u'New York': 'NY',
-        u'Vermont' : 'VT',
-        u'Connecticut': 'CT',
-        u'Massachusetts': 'MA',
-        u'New Hampshire': 'NH',
-        u'Maine': 'ME',
-        u'Rhode Island': 'RI',
-        # Got this far and forgot Delware... so close
-        u'Delaware': 'DE',
-        u'District of Columbia': 'DC',
-        u'Puerto Rico': 'PR',
-        u'Virgin Islands': 'VI',
-        u'Guam': 'GU',
-        u'Northern Mariana Islands': 'MP',
-        u'American Samoa': 'AS',
-        u'Micronesia': 'FM',
-        u'Marshall Islands': 'MH',
-        u'Palau': 'PW',
-        u'U.S. Military – Americas': 'AA',
-        u'U.S. Military – Europe': 'AE',
-        u'U.S. Military – Pacific': 'AP',
-        u'British Columbia': 'BC',
-        u'Manitoba': 'MB',
-        u'New Brunswick': 'NB',
-        u'Quebec': 'QC',
-        u'Northern Territories': 'NT',
-        u'Nunavut': 'NU',
-        u'Alberta': 'AB',
-        u'Newfoundland and Labrador': 'NL',
-        u'Nova Scotia': 'NS',
-        u'Ontario': 'ON',
-        u'Prince Edward Island': 'PE',
-        u'Saskatchewan': 'SK',
-        u'Yukon': 'YT'
-    }
-
-
 class MailingAddress(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     brother_id = db.Column(db.Integer, db.ForeignKey('brother.id'))
-    line_1 = db.Column(db.String(100))
-    line_2 = db.Column(db.String(100))
-    city = db.Column(db.String(80))
-    postal_code = db.Column(db.String(20))
-    state = db.Column(db.Enum(*STATES.values()))
+    address = db.Column(db.Text)
     description = db.Column(db.String(60))
 
