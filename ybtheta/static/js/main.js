71502b66b6ea09dee301df15af0c41473092a735
Will Ross <paxswill@gmail.com>
2013-02-12 02:20:18 -0500

    Toggle between view and edit modes for Brothers

--- ybtheta/static/js/main.js
+++ ybtheta/static/js/main.js
@@ -1,6 +1,14 @@
 $(function() {
 	$('a#edit').bind('click', function() {
-		$('dl').css('display', 'none');
+		$('dl.edit-toggle').css('display', 'none');
+		$('form.edit-toggle').css('display', 'inherit');
+		return false;
+	});
+});
+$(function() {
+	$('a#cancel').bind('click', function() {
+		$('dl.edit-toggle').css('display', 'inherit');
+		$('form.edit-toggle').css('display', 'none');
 		return false;
 	});
 });
