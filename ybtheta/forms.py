69c0a97eda425495257cc7e6b0957be408431728
Will Ross <paxswill@gmail.com>
2013-02-12 15:25:21 -0500

    Expose the more complicated brother fields in UI
    They probably don't do anything, and the styling
    is crappy, but it's a start.

--- ybtheta/forms.py
+++ ybtheta/forms.py
@@ -1,6 +1,35 @@
-from flask.ext.wtf import (Form, TextField, TextAreaField, DateField,
-        HiddenField, Required, Optional)
+from flask.ext.wtf import (Form, BooleanField, DateField, FieldList,
+        FormField, HiddenField, RadioField, SelectField, TextField,
+        TextAreaField, Email, Optional, Required)
 from flask.ext.wtf.html5 import EmailField, TelField, IntegerField
+from wtforms import Form as WTForm
+
+from ybtheta.models import Position
+
+
+class PositionForm(WTForm):
+    id = HiddenField()
+    position = SelectField('Position', choices=
+            [(val, val) for val in Position.POSITIONS.keys()])
+    date = DateField('Start Date', validators=[Optional()])
+    current = BooleanField('Current', default=False, validators=[Required()])
+
+
+class BrotherContactInfo(WTForm):
+    id = HiddenField()
+    description = TextField('Description')
+
+
+class BrotherEmail(BrotherContactInfo):
+    email = EmailField('Email address', validators=[Email()])
+
+
+class BrotherPhoneNumber(BrotherContactInfo):
+    phone_number = TelField('Telephone number')
+
+
+class BrotherAddress(BrotherContactInfo):
+    address = TextAreaField('Mailing address')
 
 
 class BrotherForm(Form):
@@ -16,13 +45,15 @@ class BrotherForm(Form):
     initiation = DateField('Initiation Date', validators=[Optional()])
     pledge_class = TextField('Pledge Class')
     # big_brother
-    # current_positions
-    # past_positions
+    current_positions = FieldList(FormField(PositionForm))
+    past_positions = FieldList(FormField(PositionForm))
+    status = SelectField('Status', choices=[(val, val) for val in ('Alumni',
+        'Student', 'Co-Op', 'Inactive', 'Pledge')])
     graduation_date = DateField('Graduation Date', validators=[Optional()])
     major = TextField('Major')
-    # emails
-    # phone_numbers
-    # addresses
+    emails = FieldList(FormField(BrotherEmail))
+    phone_number = FieldList(FormField(BrotherPhoneNumber))
+    addresses = FieldList(FormField(BrotherAddress))
     quotes = TextAreaField('Quotes')
 
 
