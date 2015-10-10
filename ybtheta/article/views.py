f30246ff5c3765254018f55709eb55b2f6ec9f54
Will Ross <paxswill@gmail.com>
2015-10-10 18:03:13 -0400

    Add a view to list all articles

--- ybtheta/article/views.py
+++ ybtheta/article/views.py
@@ -1,11 +1,17 @@
 from __future__ import absolute_import
 
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template
 
 from .models import Article
 
 
 blueprint = Blueprint('article', 'ybtheta.article', template_folder='templates')
+
+
+@blueprint.route('/')
+def list_articles():
+    articles = Article.query.all()
+    return render_template('list_articles.html', articles=articles)
 
 
 @blueprint.route('/<int:article_id>/')
