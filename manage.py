52183be8d2cbfd855ca030cdf1a9c003d6a6ee6c
Will Ross <paxswill@gmail.com>
2015-10-10 00:21:33 -0400

    Actually use the configuration file for the app

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
 
+import argparse
+import os
+import os.path
+
 from flask import Flask
-from flask.ext.script import Manager
+from flask.ext import script
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from ybtheta import create_app
@@ -16,7 +20,7 @@ migrate = Migrate(None, None)
 # manager that overrides the __call__ method. The MigrateManager.__call__
 # method initializes the app if needed, and then at the same time initializes
 # the migration object (an instance of Migrate).
-class MigrateManager(Manager):
+class MigrateManager(script.Manager):
 
     def __call__(self, app=None, **kwargs):
         if app is None:
@@ -33,9 +37,31 @@ class MigrateManager(Manager):
 
 manager = MigrateManager(create_app)
 manager.add_command('db', MigrateCommand)
-manager.add_option('-c', '--config', dest='config', required=False)
+
+
+class AbsolutePathAction(argparse.Action):
+    """Custom argparse.Action that transforms path strings into absolute paths.
+    """
+    def __call__(self, parser, namespace, values, option_string=None):
+        current_absolute = os.path.abspath(os.getcwd())
+        if isinstance(values, str):
+            new_values = os.path.join(current_absolute, values)
+        else:
+            new_values = []
+            for value in values:
+                real_path = os.path.join(current_absolute, values)
+                new_values.append(real_path)
+        setattr(namespace, self.dest, new_values)
+
+
+# Monkeypatch Flask-Script to consider my custom path action 'safe'
+safe_actions = list(script.safe_actions)
+safe_actions.append(AbsolutePathAction)
+script.safe_actions = safe_actions
+# Add option for pointing to a configuration file
+manager.add_option('-c', '--config', dest='config', required=False,
+        action=AbsolutePathAction)
 
 
 if __name__ == '__main__':
-    migrate.init_app
     manager.run()
--- ybtheta/app.py
+++ ybtheta/app.py
@@ -1,13 +1,24 @@
 from __future__ import absolute_import
 
 from flask import Flask
+import six
 
 from . import base
 from .auth import blueprint
 from .database import db
 
 def create_app(config=None, **kwargs):
-    app = Flask('ybtheta')
+    app = Flask('ybtheta', **kwargs)
+    # Configure the app using the passed in config
+    if isinstance(config, dict):
+        app.config.update(config)
+    elif isinstance(config, six.string_types):
+        if config.endswith(('.txt.', '.py', '.cfg')):
+            app.config.from_pyfile(config)
+        else:
+            app.config.from_object(config)
+    # initialize the various plugins and extensions
     db.init_app(app)
+    # Add the blueprints
     app.register_blueprint(base.blueprint)
     return app
